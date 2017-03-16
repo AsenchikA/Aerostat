17901a6bf3be252a36d3783663aed4a0c972cdd8
Asenchik Anastasiya <n-asenchik@yandex.ru>
2017-03-16 11:55:15 +0400

    Добавлены переменные

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 
+//подгрузка шрифтов
   loadFont(
   'FiraSans',                      // название шрифта
- '/css/font_firasans_woff.css?v=1',   // откуда с сервака тянуть CSS с закодированным в base64 форматом WOFF
-  '/css/font_firasans_woff2.css?v=1'   // откуда с сервака тянуть CSS с закодированным в base64 форматом WOFF2
+ '/css/font_firasans_woff.css?v=1',   // откуда тянуть CSS с закодированным в base64 форматом WOFF
+  '/css/font_firasans_woff2.css?v=1'   // откуда тянуть CSS с закодированным в base64 форматом WOFF2
 );
 });

49ef3adc558067f0bacf19d65c1e7cb09cf30e76
Asenchik Anastasiya <n-asenchik@yandex.ru>
2017-03-20 13:10:23 +0400

    Стилизация header, promo

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,17 @@ $(document).ready(function(){
 
 //подгрузка шрифтов
   loadFont(
-  'FiraSans',                      // название шрифта
- '/css/font_firasans_woff.css?v=1',   // откуда тянуть CSS с закодированным в base64 форматом WOFF
-  '/css/font_firasans_woff2.css?v=1'   // откуда тянуть CSS с закодированным в base64 форматом WOFF2
-);
+    'FiraSans',                      // название шрифта
+    '/css/font_firasans_woff.css?v=1',   // откуда тянуть CSS с закодированным в base64 форматом WOFF
+    '/css/font_firasans_woff2.css?v=1'   // откуда тянуть CSS с закодированным в base64 форматом WOFF2
+  );
+
+  // бургер в header
+  var toggler = document.getElementById('toggler');
+  toggler.onclick = function(e){
+  e.preventDefault();
+  toggler.classList.toggle('toggler--close');
+
+  document.getElementById('nav').classList.toggle('main-nav--visible');
+}
 });
